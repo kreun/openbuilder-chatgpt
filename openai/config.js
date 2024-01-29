997ea96d093f117bd6bec7faea17b430cad4e4e2
kim yoonhyoung <kimyoonhyoung@kimui-Macmini.local>
2024-01-29 09:53:53 +0900

    env

--- openai/config.js
+++ openai/config.js
@@ -1,10 +1,12 @@
 
+require("dotenv").config();
+console.log("OPEN_AI:", process.env.OPEN_AI);
 // 모델은 3.5 혹은 4를 사용한다. 모델이 계속 변경되고, 최신 모델을 사용하는 것이 유리.
 
 const config = {
     url: "https://api.openai.com/v1/chat/completions",
     embedding_url: "https://api.openai.com/v1/embeddings",
-    key: "sk-",
+    key: process.env.OPEN_AI,
     chat_model : "gpt-3.5-turbo-1106"
 }
 
